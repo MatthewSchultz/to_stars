23ddee5975164f5bc859fd84db6235b10351a29f
Matthew Schultz <abstractapproach@gmail.com>
2023-01-08 12:21:24 -0800

    init

---
+++ lib/to_stars/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module ToStars
+  VERSION = "0.1.0"
+end
---
+++ spec/to_stars_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe ToStars do
+  it "has a version number" do
+    expect(ToStars::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
